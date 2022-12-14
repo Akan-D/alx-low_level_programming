f1702bc2d2a2a8a3ec19724bcbd85f3d6096e2de
Akan_D <akanimogoodnessudoh.com>
2022-12-14 12:16:07 -0800

    good news

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                   
- * main - Entry point                                                                                                                
- * Return: Always 0 (Success)           
- * */                                                                                                                                     
+#include <studio.h>
 
-                                                                                                                                    
-int main(void)                                                                                                                          
-{                                                                                                                                       
-
-	        printf("with proper grammar, but the outcome is a piece of art,");                                                                   
-
-		        return (0);                                                                                                   
-			                                                                                                                                    
-}  
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
