2472b7b9e1aa1bd678628ae295661cef8d9620e2
Akan_D <akanimogoodnessudoh.com>
2022-12-14 13:22:36 -0800

    cprogram

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - project on size of byte
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+	int no;
+	long int lo;
+	long long int loo;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(s)\n",  (unsigned long)sizeof(no));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(lo));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(loo));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
