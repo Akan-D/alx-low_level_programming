c5038c0c7757b691a5aca873221b9596c593aa5b
Akan_D <akanimogoodnessudoh.com>
2022-12-19 05:31:55 -0800

    6th

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - num6
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no >= 10, no++)
+		putchar((num % 10) + '0');
+	printf('\n');
+	return (0);
+}
