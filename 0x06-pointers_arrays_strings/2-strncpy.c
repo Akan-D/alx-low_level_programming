6839df6a4602380bf7d178bac6f2bf9439bde3c0
Akan_D <akanimogoodnessudoh.com>
2022-12-22 05:07:13 -0800

    fin

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,14 +2,16 @@
 
 /**
  * _strncpy - 2nd time
- *
+ *@dest: now
+ *@src: nn
+ *@n: no
  * Return: 0
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n && *(src + i); i++)
 	{
 		*(dest + i) = *(src + i);
