6e72fe354ee7f9dc2d7ed535c3cf53b7d23f4210
Akan_D <akanimogoodnessudoh.com>
2022-12-28 14:06:50 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - 100th
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (x = y + 1; y < 10; y++)
+		{
+			putchar((x % 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x == 8 && y == 9)
+				continue;
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
