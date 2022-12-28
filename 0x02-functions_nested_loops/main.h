3ae9c7cdabdb835028036676e6721634a571f492
Akan_D <akanimogoodnessudoh.com>
2022-12-28 15:19:08 -0800

    #ifndef MAIN_H

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H                                                     
+
+#define MAIN_H
+/*main h*/                                                  
+                                                                   
+                                               
+int print_alphabet();
+#endif
