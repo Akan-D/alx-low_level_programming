3eac75e77b46dbfea0c0e3444c23d593bf930c01
Akan_D <akanimogoodnessudoh.com>
2023-01-10 14:32:44 -0800

    new

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- *print_rev - print
- *@s: string
- *Return: num
- */
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+*/
+
 void print_rev(char *s)
 {
-	int i = 0;
+	int i, n;
 
-	while (str[i] != '\0')
+	n = 0;
+
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
